990678239f6dfda08462206844825b1dfac29536
Naren Sundaravaradan <nano.naren@gmail.com>
2011-05-05 09:47:33 -0400

    normalizeByMax

--- NanoUtils/Container.hs
+++ NanoUtils/Container.hs
@@ -1,10 +1,11 @@
 module NanoUtils.Container
     (
       normalize
+    , normalizeByMax
     , foldMapM
     ) where
 
-import Prelude hiding (foldr)
+import Prelude hiding (foldr,maximum)
 import Data.Foldable
 import Data.Monoid
 import Control.Monad
@@ -14,6 +15,12 @@ import Control.Monad
 normalize :: (Foldable t,Functor t,Fractional a) => t a -> t a
 normalize t = let total = foldl' (+) 0 t
               in fmap (/total) t
+
+-- | Normalize values in a container, that is, each value is
+-- divided by the max value in it
+normalizeByMax :: (Foldable t,Functor t,Fractional a,Ord a) => t a -> t a
+normalizeByMax t = let max = maximum t
+                   in fmap (/max) t
 
 -- | Monaidic version of foldMap
 foldMapM :: (Foldable t,Monoid v,Monad m) => (a -> m v) -> t a -> m v
